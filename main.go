864898e9e972b49b200b89f4817db1f937b288f9
DESKTOP-OJHNPHQ\ali <iletisim@alikaya.net.tr>
2017-05-18 22:24:46 +0300

    nil pointer

--- main.go
+++ main.go
@@ -16,34 +16,45 @@ func hello(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "Client açılamadı.")
 	}
 	
+	else{
 	
-	exists, err := client.IndexExists("content").Do(context.Background())
-	if err != nil {
-		io.WriteString(w, "Index Açılırken hata oldu.")
-	}
-	if !exists {
-		// Index does not exist yet.
-		io.WriteString(w, "Index bulunamadı.")
-	}
-	
-	termQuery := elastic.NewTermQuery("text", "Dandanakan")
-	searchResult, err := client.Search().
-						Index("content").        // search in index "twitter"
-						Query(termQuery).        // specify the query
-						//Sort("user", true).      // sort by "user" field, ascending
-						From(0).Size(10).        // take documents 0-9
-						Pretty(true).            // pretty print request and response JSON
-						Do(context.Background()) // execute
-	
-	if err != nil {
-		// Handle error
-		io.WriteString(w, "Search hata oldu.")
-	}
+		exists, err := client.IndexExists("content").Do(context.Background())
+		if err != nil {
+			io.WriteString(w, "Index Açılırken hata oldu.")
+		}
+		if !exists {
+			// Index does not exist yet.
+			io.WriteString(w, "Index bulunamadı.")
+		}
+		else{
+		
+			termQuery := elastic.NewTermQuery("text", "Dandanakan")
+			searchResult, err := client.Search().
+								Index("content").        // search in index "twitter"
+								Query(termQuery).        // specify the query
+								//Sort("user", true).      // sort by "user" field, ascending
+								From(0).Size(10).        // take documents 0-9
+								Pretty(true).            // pretty print request and response JSON
+								Do(context.Background()) // execute
+			
+			if err != nil {
+				// Handle error
+				io.WriteString(w, "Search hata oldu.")
+			}
 
-	// searchResult is of type SearchResult and returns hits, suggestions,
-	// and all kinds of other information from Elasticsearch.
-	s := fmt.Sprintf("Query took milliseconds : %d ", searchResult.TookInMillis)
-	io.WriteString(w,s)
+			else{
+			
+				// searchResult is of type SearchResult and returns hits, suggestions,
+				// and all kinds of other information from Elasticsearch.
+				s := fmt.Sprintf("Query took milliseconds : %d ", searchResult.TookInMillis)
+				io.WriteString(w,s)
+			}
+			
+		}
+		
+		
+	}
+	
 
 
 }
